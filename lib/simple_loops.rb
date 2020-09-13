bd9755aa138f34e2cd48e825598a2793383d4e9a
wintanagb <wintanaltc@gmail.com>
2020-09-13 17:08:13 -0500

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,7 @@
 def loop_message_five_times (message)
    count = 0
-   puts message
    while count < 5
+    puts message
     count+= 1   
     end
 end
@@ -22,3 +22,11 @@ def output_array (array)
     end
 end 
 
+def return_string_array(array)
+    count = 0
+    while count < array.length do
+      array[count] = array[count].to_s
+      count += 1
+    end
+    array
+  end
