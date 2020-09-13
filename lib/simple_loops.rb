c7567717b9d81cfa047b8254474de38680c5a424
wintanagb <wintanaltc@gmail.com>
2020-09-13 17:04:17 -0500

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -2,7 +2,7 @@ def loop_message_five_times (message)
    count = 0
    puts message
    while count < 5
-    count=+ 1   
+    count+= 1   
     end
 end
 
@@ -10,7 +10,7 @@ def loop_message_n_times (message,limit)
     count = 0
     while count < limit do
         puts message
-        count=+ 1
+        count+= 1
     end
 end
 
@@ -18,7 +18,7 @@ def output_array (array)
     count = 0
     while count < array.length do
         puts array [count]
-        count=+ 1
+        count+=1
     end
 end 
 
