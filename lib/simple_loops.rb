d006c64778d8c8d0885453da5bcece17c3786795
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-17 17:10:53 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,26 @@
 # Write your methods here
+
+def loop_message_five_times (message)
+  5.times do
+    puts (message)
+  end
+end
+
+def loop_message_n_times (message, int)
+  int.times do
+    puts (message)
+  end
+end
+
+def output_array (array)
+  for i in array do
+    puts (i)
+  end
+end
+
+def return_string_array (array)
+  new_array = []
+  array.each{|i| new_array.push(i.to_s)}
+  new_array
+end
+
